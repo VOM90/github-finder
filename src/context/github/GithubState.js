7e0c26b2236081780286b85ee2e3545ea6ec983f
VOM90 <lamarcus@ukr.net>
2019-08-28 21:19:29 +0300

    moving respos state to context

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -41,6 +41,17 @@ const GithubState = props => {
     });
   };
 
+  const getUserRepos = async username => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_CLIENT_SECRET}`
+    );
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
+
   const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
   const setLoading = () => dispatch({ type: SET_LOADING });
@@ -54,7 +65,8 @@ const GithubState = props => {
         loading: state.loading,
         searchUsers,
         clearUsers,
-        getUser
+        getUser,
+        getUserRepos
       }}
     >
       {props.children}
